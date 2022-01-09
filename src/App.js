e5f82a2b73f8bce6aa2e9f1b9337fcde27fb81c2
Stefp27 <stefania.p27@gmail.com>
2022-01-09 15:02:03 +0100

    Imported App Data and displayed current song

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/app.scss"
 import Song from "./components/Song";
 import Player from "./components/Player";
+import data from "./components/util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong}/>
+      <Player 
+      setIsPlaying={setIsPlaying}
+      isPlaying={isPlaying}
+      currentSong={currentSong}/>
     </div>
   );
 }
---
+++ src/components/util.js
@@ -0,0 +1,79 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Later today",
+      cover: "https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/61226ba21704e25a2ece5728_tet0c8gdl30scqu.png",
+      artist: "ØDYSSEE, Afroham",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21745",
+      color: ["#7F99A5", "#EE977D"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Perfect Storm",
+      artist: "Aviino",
+      cover: "https://chillhop.com/wp-content/uploads/2021/09/a2d710149876c8e0c9fb097840b2438fbc21bd37-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#D97542", "#5D7A8C"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=23057",
+    },
+    {
+      name: "Setting Up Our Beds in Minecraft",
+      artist: "Sleepy Fish",
+      cover: "https://chillhop.com/wp-content/uploads/2021/09/2ce75252f5419a45d76bb93424ac1eae3e688b17-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#118EE8", "#FDEEE1"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=24826",
+    }
+  ];
+}
+
+export default chillHop;
