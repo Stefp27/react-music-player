a88af83c86fff5094e0412d57ac3af13bf54cc8c
Stefp27 <stefania.p27@gmail.com>
2022-01-10 16:53:02 +0100

    New font added; Created Library section for displaying all the songs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/app.scss"
 import Song from "./components/Song";
 import Player from "./components/Player";
 import data from "./components/util";
+import Library from "./components/Library";
 
 function App() {
   const [songs, setSongs] = useState(data());
@@ -16,6 +17,7 @@ function App() {
       setIsPlaying={setIsPlaying}
       isPlaying={isPlaying}
       currentSong={currentSong}/>
+      <Library songs={songs}/>
     </div>
   );
 }
